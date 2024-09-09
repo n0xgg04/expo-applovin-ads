c80dafdfacbc6424bc782389359141b0030cbd1d
Luong Tuan Anh <dulieu.vblc@gmail.com>
2024-09-09 23:37:22 +0700

    A

--- plugin/src/index.js
+++ plugin/src/index.js
@@ -1,7 +1,7 @@
-const { withApplovinProjectGradle } = require("./withApplovinBuildGradle");
-const { withApplovinGradle } = require("./withApplovinGradle");
-const { withApplovinManifest } = require("./withApplovinManifest");
-const { withAppLovinPodfile } = require("./withDangerousMod");
+const withApplovinProjectGradle = require("./withApplovinBuildGradle");
+const withApplovinGradle = require("./withApplovinGradle");
+const withApplovinManifest = require("./withApplovinManifest");
+const withAppLovinPodfile = require("./withDangerousMod");
 
 module.exports = function withApplovinPlugin(config, data) {
   return withApplovinProjectGradle(
